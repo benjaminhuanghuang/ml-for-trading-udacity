945c5c044e4500f88c1b751988a6322b958eb19a
Benjamin Huang <bhuang@rms.com>
2018-02-17 14:03:47 -0800

    create project

---
+++ README.md
@@ -0,0 +1,15 @@
+## Reference
+- https://classroom.udacity.com/courses/ud501
+- https://github.com/iamnewneo/ML_For_Trading
+
+# Setup
+```
+$  virtualenv --system-site-packages venv   # for Python 2.7
+
+$ . venv3/bin/activate
+
+(venv3)$ pip install -r requirements.txt
+
+...
+(venv3)$ deactivate
+```
---
+++ stock_csv.py
@@ -0,0 +1,38 @@
+import pandas as pd
+
+def symbol_to_path(symbol, base_dir='data'):
+    return os.path.join(base_dir, '{}.csv'.format(str(symbol)))
+
+def get_data(symbols, dates):
+    """Read stock data (adjusted close) for given symbols from CSV files."""
+    df = pd.DataFrame(index=dates)
+    if 'SPY' not in symbols:  # add SPY for reference, if absent
+        symbols.insert(0, 'SPY')
+
+    for symbol in symbols:
+        # TODO: Read and join data for each symbol
+        path = symbol_to_path(symbol)
+        df_tmp = pd.read_csv(path, index_col="Date", parse_dates=True,
+                         usecols=['Date', 'Adj Close'], na_values=['nan'])
+        df_tmp = df_tmp.rename(columns={'Adj Close':symbol})
+        # or use inner join to drop the NaN values
+        df = df.join(df_tmp, how='inner')
+    return df
+
+
+def test_run():
+    # define date range
+    start_date = '2010-01-22'
+    end_date = '2010-10-26'
+    dates = pd.date_range(start_date, end_date)
+
+    # Choose stock symbols to read
+    symbols = ['GOOG', 'IBM', 'GLD']
+    
+    # Get stock data
+    df = get_data(symbols, dates)
+    print df
+
+
+if __name__ == "__main__":
+    test_run()
